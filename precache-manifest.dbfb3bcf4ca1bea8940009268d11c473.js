ff8e25086a85a1f4ccf33e910ff1e73a0a5a24fe
Fazrin <fazrin.mutaqin@gmail.com>
2022-06-21 11:21:50 +0700

    Updates

---
+++ precache-manifest.dbfb3bcf4ca1bea8940009268d11c473.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd5ab723c01e6e93e85",
+    "url": "/static/css/main.81b27c41.chunk.css"
+  },
+  {
+    "revision": "abd5ab723c01e6e93e85",
+    "url": "/static/js/main.abd5ab72.chunk.js"
+  },
+  {
+    "revision": "41211fcf000cf3a9973c",
+    "url": "/static/js/1.41211fcf.chunk.js"
+  },
+  {
+    "revision": "56c2511f1f6564f3d7ab",
+    "url": "/static/css/2.fbc62d61.chunk.css"
+  },
+  {
+    "revision": "56c2511f1f6564f3d7ab",
+    "url": "/static/js/2.56c2511f.chunk.js"
+  },
+  {
+    "revision": "39a7286a869c5b2ec7f4",
+    "url": "/static/js/runtime~main.39a7286a.js"
+  },
+  {
+    "revision": "78ab00a09e4914c2b811041e8cd22216",
+    "url": "/static/media/IMG_20220102_164245v5.78ab00a0.jpg"
+  },
+  {
+    "revision": "b2386394b03c82ee90012c00f7643849",
+    "url": "/static/media/profile-pict-2.b2386394.jpg"
+  },
+  {
+    "revision": "6195e6a7f558f550f355c1ec8fa286da",
+    "url": "/static/media/resume-fazrin-v4.6195e6a7.pdf"
+  },
+  {
+    "revision": "eadddf05603e110b01bccd96fc0a401d",
+    "url": "/static/media/FuturaLT-Book.eadddf05.woff"
+  },
+  {
+    "revision": "27eca0dd8380d38f51eb305c99595c3d",
+    "url": "/static/media/pantai.27eca0dd.jpg"
+  },
+  {
+    "revision": "bb9f93f69b195b6660ac48840a9f66fb",
+    "url": "/static/media/starry-night-sky.bb9f93f6.jpg"
+  },
+  {
+    "revision": "5cc4343bdbdaaf5b8d45d06bdec7c08b",
+    "url": "/index.html"
+  }
+];
